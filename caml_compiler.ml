0a7a223452ab64b73e239356e42d2efa56fe2ab6
VILA Rodrigo <r94vila@gmail.com>
2025-05-07 14:49:15 +0200

    feat: completed function compile

--- caml_compiler.ml
+++ caml_compiler.ml
@@ -44,6 +44,60 @@ and value =
 and operator = Add | Sub | Mult
 
 (* Figure 10 page 24 (Translation from Mini-ML to CAM) *)
-let compile (e:expr) : coms =
+let rec compile (e: expr) (env : (ident * value) list) : coms =
   match e with
-  | _ -> failwith "todo"
+  | Number n -> [Quote (Int n)]                          (* 2 *)
+  | True -> [Quote (Bool true)]                          (* 3 *)
+  | False -> [Quote (Bool false)]                        (* 4 *)
+  | Ident i ->                                           (* 5 *) (* à verif *)
+      let value = List.assoc i env in 
+      [Quote(value)]
+
+  | If (e1, e2, e3) ->                                   (* 6 *)
+      let c1 = compile e1 env in
+      let c2 = compile e2 env in
+      let c3 = compile e3 env in
+      Push :: c1 @ [Branch (c2, c3)]
+
+  | Mlpair (e1, e2) ->                                   (* 7 *)
+      let c1 = compile e1 env in
+      let c2 = compile e2 env in
+      [Push] @ c1 @ [Swap] @ c2 @ [Cons]
+
+  | Let (p, e1, e2) ->                                   (* 8 *)
+      let c1 = compile e1 env in
+      let new_env = match p with
+        | IdentPat(x) -> (x, NullValue) :: env
+        | _ -> env 
+      in
+      let c2 = compile e2 new_env in
+      Push :: c1 @ [Cons] @ c2
+
+  | LetRec (p, e1, e2) ->                                (* 9 *)
+      let new_env = match p with
+        | IdentPat(x) -> (x, NullValue) :: env
+        | _ -> env
+      in
+      let c1 = compile e1 new_env in
+      let c2 = compile e2 new_env in
+      [Push; Quote(NullValue); Cons; Push] @ c1 @ [Swap; Rplac] @ c2
+
+  | Lambda (p, e) ->                                    (* 10 *)
+      let new_env = match p with
+        | IdentPat(x) -> (x, NullValue) :: env
+        | _ -> env 
+      in
+      let c = compile e new_env in
+      [Cur(c)]
+
+  | Apply (e1, e2) ->                                   (* 12 *)
+      match e1 with
+      | Ident("add") -> compile e2 env @[Op(Add)]
+      | Ident("sub") -> compile e2 env @[Op(Sub)]
+      | Ident("mult") -> compile e2 env @[Op(Mult)]
+      | _ -> 
+          let c1= compile e1 env in 
+          let c2= compile e2 env in 
+          Push :: c1 @ [Swap] @ c2 @ [Cons;App]
+
+  
